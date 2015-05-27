2a1e9a7fe8e7be1f2a4601b70510a3da78e3f7b4
Linus Schrewe <linus.schrewe@gmail.com>
2015-05-27 23:15:24 +0200

    Stil: Überflüssige `public` Deklarationen entfernt.

--- src/main/java/de/ostmarkbeuthen/kasse/database/Event.java
+++ src/main/java/de/ostmarkbeuthen/kasse/database/Event.java
@@ -3,7 +3,7 @@ package de.ostmarkbeuthen.kasse.database;
 import java.util.Date;
 
 public interface Event {
-	public Date getBegin();
-	public Date getEnd();
-	public void setCost(Product prod, int cost);
+	Date getBegin();
+	Date getEnd();
+	void setCost(Product prod, int cost);
 }
